453f5ab79ff8d276f72ee5aa6ae3ef8ff4817464
twentiecker <twentiecker@gmail.com>
2025-07-09 23:20:11 +0700

    add money-tracker

--- money-tracker/application/config/config.php
+++ money-tracker/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // TODO: Sesuaikan dengan base url front-end yang digunakan
-$config['base_url'] = 'http://frontend.submission-mgce-learifandhi.et.r.appspot.com';
+// $config['base_url'] = 'http://frontend.submission-mgce-learifandhi.et.r.appspot.com';
 
 /*
 |--------------------------------------------------------------------------
